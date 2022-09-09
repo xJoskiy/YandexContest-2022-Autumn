65ccfd85d2e00d3eeccb2ecace3c24f09ee8464f
xJoskiy <pavel5553-20@mail.ru>
2022-09-10 00:39:12 +0300

    commit

---
+++ D.py
@@ -0,0 +1,34 @@
+def search(plan, x, y, direction=None):
+    if plan[x + 1][y] == '0':
+        plan[x + 1] = plan[x + 1][:y] + 'U' + plan[x + 1][y + 1:]
+        search(plan, x + 1, y)
+
+    if plan[x - 1][y] == '0':
+        plan[x - 1] = plan[x - 1][:y] + 'D' + plan[x - 1][y + 1:]
+        search(plan, x - 1, y)
+
+    if plan[x][y + 1] == '0':
+        plan[x] = plan[x][:y + 1] + 'L' + plan[x][y + 2:]
+        search(plan, x, y + 1)
+
+    if plan[x][y - 1] == '0':
+        plan[x] = plan[x][:y - 1] + 'R' + plan[x][y:]
+        search(plan, x, y - 1)
+
+
+def main():
+    N, M = map(int, input().split())
+    plan = [input().replace('.', '0') for _ in range(N)]
+
+    start_x, start_y = 0, 0
+    for i in range(1, N - 1):
+        for j in range(1, M - 1):
+            if plan[i][j] == 'S':
+                start_x, start_y = i, j
+    search(plan, start_x, start_y)
+
+    print(*plan, sep="\n")
+
+
+if __name__ == '__main__':
+    main()
